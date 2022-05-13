82cc28c07cd78d33bf4e1a3737285a1368c418b6
Anthony Briglia <9729776+ambriglia@users.noreply.github.com>
2022-05-13 14:07:13 -0400

    first

---
+++ diagonalDifference.js
@@ -0,0 +1,16 @@
+const diagonalDifference = (arr) => {
+  const length = arr[0][0];
+  let sum1 = 0;
+  let sum2 = 0;
+
+  for (let i = 0; i < length; i++) {
+    const num1 = arr[i + 1][i];
+    sum1 += num1;
+    const num2 = arr[i + 1][length - 1 - i];
+    sum2 += num2;
+  }
+
+  return Math.abs(sum1 - sum2);
+};
+
+module.exports = diagonalDifference;
---
+++ diagonalDifference.test.js
@@ -0,0 +1,16 @@
+const diagonalDifference = require('./diagonalDifference');
+
+describe('diagonalDifference', () => {
+  it('should calculate the diagonal difference', () => {
+    const input = [
+      [3],
+      [11, 2, 4],
+      [4, 5, 6],
+      [10, 8, -12]
+    ];
+
+    const result = diagonalDifference(input);
+
+    expect(result).toEqual(15);
+  })
+})
